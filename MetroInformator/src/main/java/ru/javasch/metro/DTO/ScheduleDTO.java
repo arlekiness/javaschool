5e8e77543995c2a5680f4c1132e17f63eb400a21
Alexey Bystrov <volpert13@gmail.com>
2018-09-01 20:26:24 +0300

    Login and password works

--- MetroInformator/src/main/java/ru/javasch/metro/DTO/ScheduleDTO.java
+++ MetroInformator/src/main/java/ru/javasch/metro/DTO/ScheduleDTO.java
@@ -13,22 +13,17 @@ public class ScheduleDTO {
     private String endPointStationName;
 
     public String getDateDeparture() { return dateDeparture; }
-
     public void setDateDeparture(String dateDeparture) { this.dateDeparture = dateDeparture; }
 
     public String getDateArrival() { return dateArrival; }
-
     public void setDateArrival(String dateArrival) { this.dateArrival = dateArrival; }
 
     public String getTrainName() { return trainName; }
-
     public void setTrainName(String trainName) { this.trainName = trainName; }
 
     public String getStation() { return station; }
-
     public void setStation(String station) { this.station = station; }
 
     public String getEndPointStationName() { return endPointStationName; }
-
     public void setEndPointStationName(String endPointStationName) { this.endPointStationName = endPointStationName; }
 }
